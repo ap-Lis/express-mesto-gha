61f6d22781c699a7176e8b16a63c845288f42c67
Gleb <gleb@grib.in>
2023-07-29 16:20:28 +0300

    13th test not valid urls

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const NOT_FOUND_ERROR_CODE = 404;
 
 const { PORT = 3000 } = process.env;
 
@@ -23,6 +24,9 @@ app.use((req, res, next) => {
 
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
+app.use((req, res) => {
+  res.status(NOT_FOUND_ERROR_CODE).send({message: "URL не найден"})
+})
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
