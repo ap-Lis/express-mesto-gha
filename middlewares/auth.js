e901e07409df52818a71f7fdec27f38c180a6024
Gleb <gleb@grib.in>
2023-08-13 00:20:05 +0300

    14th test 3rd try

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,14 +4,14 @@ const UnauthorizedError = require('../errors/unauthorized-err');
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    next(new UnauthorizedError('Неправильный email или пароль'));
+    next(new UnauthorizedError('Необходима авторизация'));
   }
   const token = authorization.replace('Bearer ', '');
   let payload;
   try {
     payload = jwt.verify(token, 'a-man-a-plan-a-canal-panama');
   } catch (err) {
-    next(new UnauthorizedError('Неправильный email или пароль'));
+    next(new UnauthorizedError('Ошибка проверки jwt токена'));
   }
   req.user = payload;
   return next();
--- routes/users.js
+++ routes/users.js
@@ -13,13 +13,13 @@ router.get('/', getUsers);
 router.get('/me', getCurrentUser);
 router.get('/:id', celebrate({
   params: Joi.object().keys({
-    id: Joi.string().alphanum().length(24),
+    id: Joi.string().hex().length(24),
   }),
 }), getUser);
 router.patch('/me', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(30),
   }),
 }), updateUser);
 router.patch('/me/avatar', celebrate({
